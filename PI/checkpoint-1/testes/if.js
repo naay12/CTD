e559f32c0f6259d76daadff0edf163031ea063b5
natali <nathalysouza123z@gmail.com>
2022-03-08 20:06:19 -0300

    aula11_feita

---
+++ PI/checkpoint-1/testes/if.js
@@ -0,0 +1,22 @@
+// if ternário 
+
+var nome =  idade=19;
+
+ idade=19 ? (
+ alert("acesso liberado"),
+console.log("acesso liberado para" + nome)
+ ) : (
+    alert ("acesso negado"),
+    console.log("acesso negado para" + nome)
+ );
+
+// errinho de merda para concertar depois!
+
+
+
+
+
+
+
+
+
