754b32dd36953d68560e4efcef66d37dcfb6da1f
natali <nathalysouza123z@gmail.com>
2022-05-16 23:01:57 -0300

    primeira_aula_front2
    atividade da primeira aula de front-end 2

---
+++ FE2/A1/A1.js
@@ -0,0 +1,20 @@
+/*Desenvolver um script que permita somar um array de números consecutivos, de forma que se some o primeiro número com o segundo e o imprima através do console. 
+Depois, temos que pegar este resultado e somar o terceiro número, e assim por diante, até termos terminado de percorrer todo o array.*/
+
+/*exemplo meu*/
+let array= [1,2,4,8,16,32];
+let result=0;
+for(let i=0;i<array.length;i++){
+    result+=array[i];
+}
+console.log(result);
+
+/*exemplo do curso*/
+let n=[1,,2,4,8];
+let soma=n.reduce((i,n)=>i+=n);
+console.log(soma);
+
+
+/** Reduce:pra que ele serve?
+Como o nome sugere, o reduce busca reduzir um array. Ele iterará por cada elemento dessa lista com o objetivo de ao final gerar um único valor (de qualquer tipo), como por exemplo a soma de todos os elementos desse array. Lembrando que não ficamos presos apenas a números. */
+
---
+++ FE2/A1/a1-main.js
@@ -0,0 +1,18 @@
+/** revisão */
+
+/* variável*/
+let nome ='João';
+
+/**array-lista de strings */
+let frutas = ['Banana', 'Maçã', 'Uva'];
+
+console.log(frutas);
+console.log(nome);
+
+
+/** for-pecorrer array */
+
+for(let fruta of frutas){
+    console.log(fruta);
+}
+    
